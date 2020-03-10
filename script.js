2c96645258730e63469e7c5a912ad2a32b89475b
Gabriel Bohn <gabrielcbohn@gmail.com>
2020-03-10 15:08:30 -0300

    Colision implemented, running player, floor and ceiling customised and  game over done

--- script.js
+++ script.js
@@ -2,35 +2,48 @@ document.getElementById("start-button").onclick = () => {
   myGameArea.startGame();
 };
 
+let startGame = false;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let frames = 0;
 let obsArr = [];
+let naveArr = [];
+const SPRITE_SIZE = 36;
 
 // Images
 let img = new Image();
 img.src = "images/space-bg.jpg";
+let img2 = new Image();
+img2.src = "images/laser-sprite-png-6-180.png";
+let img4 = new Image();
+img4.src = "images/laser-sprite-png-6.png";
+let img3 = new Image();
+img3.src = "images/space-marine-run.png";
+let img5 = new Image();
+img5.src = "images/BT2001.png";
+let img6 = new Image();
+img6.src = "images/cannon-modified.png";
+let img7 = new Image();
+img7.src = "images/cannon-modified-180.png";
+let img8 = new Image();
+img8.src = "images/spaceship.png";
+let img9 = new Image();
+img9.src = "images/spaceship2.png";
+let img10 = new Image();
+img10.src = "images/space-marine-run-180.png";
 
 // declaring main objects and game parts
 // game area
 var myGameArea = {
+  points: 0,
+  topScore: 0,
   frames: 0,
-  groundY: 280,
-  y: 280,
-  height: 20,
+  groundY: 270,
+  y: 270,
+  height: 30,
   startGame: function() {
     obstacles.addObs();
     requestId = window.requestAnimationFrame(updateGameArea);
-  },
-
-  ground: function() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, this.groundY, 500, 20);
-  },
-
-  ceiling: function() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, 500, this.height);
   },
 
   clear: function() {
@@ -39,41 +52,101 @@ var myGameArea = {
 
   stop: function() {
     console.log("game over");
+    // this.clear();
+    // ctx.fillStyle = 'black';
+    // ctx.fillRect(0, 0, 500, 300);
+    ctx.beginPath();
+    ctx.font = "70px sans-serif";
+    ctx.fillStyle = "white";
+    ctx.fillText("Game Over", 75, 100);
+    ctx.font = "40px sans-serif";
+    ctx.fillText("Final Score:" + this.points, 140, 180);
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.font = "25px sans-serif";
+    ctx.fillText("Press R to restart", 140, 243);
+    obsArr = [];
+    if (this.points > this.topScore) {
+      this.topScore = this.points;
+    }
+    this.points = 0;
+    this.score(true);
     window.cancelAnimationFrame(requestId);
+  },
+
+  score: function(reset) {
+    if (reset) {
+      var points = 0;
+    }
+    var points = Math.floor(frames / 5);
+    ctx.beginPath();
+    ctx.font = "18px sans-serif";
+    ctx.fillStyle = "#ebb02c";
+    ctx.fillText("Score: " + points, 30, 20);
+    ctx.fillText("Top score:" + this.topScore, 200, 20);
+    this.points = points;
+  }
+};
+
+let ceilingImg = {
+  x: 0,
+  speed: -6,
+
+  move: function() {
+    this.x += this.speed;
+    this.x %= 500;
+  },
+
+  draw: function() {
+    ctx.drawImage(img5, this.x, 0, 500, 30);
+    if (this.speed < 0) {
+      ctx.drawImage(img5, this.x + 500, 0, 500, 30);
+    } else {
+      ctx.drawImage(img5, this.x - img.width, 0, 500, 30);
+    }
+  }
+};
+
+let floorImg = {
+  x: 0,
+  speed: -6,
+
+  move: function() {
+    this.x += this.speed;
+    this.x %= 500;
+  },
+
+  draw: function() {
+    ctx.drawImage(img5, this.x, 270, 500, 30);
+    if (this.speed < 0) {
+      ctx.drawImage(img5, this.x + 500, 270, 500, 30);
+    } else {
+      ctx.drawImage(img5, this.x - img.width, 270, 500, 30);
+    }
   }
 };
 
 // declaring player class
 class Player {
-  constructor(width, height, color, x, y) {
+  constructor(width, height, color, x, y, animation) {
+    this.animation = animation;
     this.width = width;
     this.height = height;
     this.color = color;
     this.x = x;
     this.y = y;
-    this.top = this.y;
-    this.left = this.x;
-    this.right = this.x + this.width;
-    this.bottom = this.y + this.height;
     (this.normalG = true), (this.gravity = 1.5), (this.yVelocity = 0);
-  }
-  update() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
   applyGforce() {
     this.yVelocity += this.gravity;
     this.y += this.yVelocity;
-    if (this.normalG) {
-      if (this.y > myGameArea.y - 30) {
-        this.y = myGameArea.y - 30;
-        this.yVelocity = 0;
-      }
-    } else {
-      if (this.y < myGameArea.height) {
-        this.yVelocity = 0;
-        this.y = myGameArea.height;
-      }
+    if (this.y > myGameArea.y - 30) {
+      this.y = myGameArea.y - 30;
+      this.yVelocity = 0;
+    }
+    if (this.y < myGameArea.height) {
+      this.yVelocity = 0;
+      this.y = myGameArea.height;
     }
   }
   changeG() {
@@ -84,18 +157,101 @@ class Player {
       this.normalG = true;
     }
   }
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
   crashWith(obstacle) {
-    if (this.normalG) {
+    if (this.y === 30) {
       return !(
-        this.bottom < obstacle.y ||
-        this.top > obstacle.y + obstacle.height ||
-        this.right < obstacle.x
+        this.bottom() < obstacle.y + 18 ||
+        this.top() > obstacle.y + obstacle.height - 16 ||
+        this.right() < obstacle.x ||
+        this.left() > obstacle.x + obstacle.width - 8
+      );
+    } else if (this.y === 240) {
+      return !(
+        this.bottom() < obstacle.y + 18 ||
+        this.top() > obstacle.y + obstacle.height - 16 ||
+        this.right() < obstacle.x ||
+        this.left() > obstacle.x + obstacle.width - 8
       );
     } else {
       return !(
-        this.bottom > obstacle.y ||
-        this.top < obstacle.y + obstacle.height ||
-        this.right > obstacle.x
+        this.bottom() < obstacle.y + 18 ||
+        this.top() > obstacle.y + obstacle.height - 16 ||
+        this.right() < obstacle.x + 8 ||
+        this.left() > obstacle.x + obstacle.width - 8
+      );
+    }
+  }
+}
+
+// doing player animation
+class Animation {
+  constructor(frame_set, delay) {
+    this.delay = delay;
+    this.frame = 0;
+    this.count = 0;
+    this.frame_index = 0;
+    this.frame_set = frame_set;
+  }
+
+  change(frame_set, delay = 15) {
+    if (this.frame_set != frame_set) {
+      this.count = 0;
+      this.delay = delay;
+      this.frame_index = 0;
+      this.frame_set = frame_set;
+      this.frame = this.frame_set[this.frame_index];
+    }
+  }
+
+  update() {
+    this.count++;
+
+    if (this.count >= this.delay) {
+      this.count = 0;
+
+      this.frame_index =
+        this.frame_index == this.frame_set.length - 1
+          ? 0
+          : this.frame_index + 1;
+      this.frame = this.frame_set[this.frame_index];
+    }
+  }
+  draw() {
+    if (player.normalG) {
+      ctx.drawImage(
+        img3,
+        player.animation.frame * (SPRITE_SIZE + 12),
+        10,
+        SPRITE_SIZE,
+        SPRITE_SIZE,
+        player.x,
+        player.y,
+        SPRITE_SIZE,
+        SPRITE_SIZE
+      );
+    } else {
+      ctx.drawImage(
+        img10,
+        player.animation.frame * (SPRITE_SIZE + 12),
+        10,
+        SPRITE_SIZE,
+        SPRITE_SIZE,
+        player.x,
+        player.y,
+        SPRITE_SIZE,
+        SPRITE_SIZE
       );
     }
   }
@@ -107,18 +263,17 @@ let obstacles = {
   velocity: 6,
   addObs: function() {
     let randomN = Math.floor(Math.random() * 2);
-    let randomHeight = Math.floor(Math.random() * 180);
+    let randomHeight = Math.floor(Math.random() * 110);
     if (randomN === 0) {
-      randomY = 20;
+      randomY = 30;
     } else {
-      randomY = 250 - randomHeight;
+      randomY = 180 - randomHeight;
     }
     obsArr.push({
       x: 500,
       y: randomY,
       width: 30,
-      height: 30 + randomHeight,
-      cor: "#" + Math.floor(Math.random() * 16777216).toString(16)
+      height: 90 + randomHeight
     });
   },
   moveObs: function() {
@@ -134,8 +289,54 @@ let obstacles = {
   update: function() {
     for (let i = 0; i < obsArr.length; i++) {
       let obs = obsArr[i];
-      ctx.fillStyle = obs.cor;
-      ctx.fillRect(obs.x, obs.y, obs.width, obs.height);
+      if (obs.y === 30) {
+        ctx.drawImage(img2, obs.x, obs.y, obs.width, obs.height);
+        ctx.drawImage(img7, obs.x, obs.y - 5, 30, 30);
+      } else {
+        ctx.drawImage(img4, obs.x, obs.y, obs.width, obs.height);
+        ctx.drawImage(img6, obs.x, 245, 30, 30);
+      }
+    }
+  }
+};
+
+let spaceship = {
+  velocity: 3,
+  addNav: function() {
+    randomY = Math.floor(Math.random() * 211);
+    randImg = Math.floor(Math.random() * 2);
+    if (randImg === 0) {
+      naveArr.push({
+        x: 500,
+        y: randomY + 30,
+        width: 30,
+        height: 45,
+        image: img8
+      });
+    } else {
+      naveArr.push({
+        x: 500,
+        y: randomY + 30,
+        width: 30,
+        height: 45,
+        image: img9
+      });
+    }
+  },
+  moveNav: function() {
+    for (let i = 0; i < naveArr.length; i++) {
+      let nav = naveArr[i];
+      nav.x -= this.velocity;
+      if (nav.x <= -45) {
+        naveArr.splice(i, 1);
+        i--;
+      }
+    }
+  },
+  update: function() {
+    for (let i = 0; i < naveArr.length; i++) {
+      let nav = naveArr[i];
+      ctx.drawImage(nav.image, nav.x, nav.y, 45, 30);
     }
   }
 };
@@ -145,34 +346,38 @@ let controller = {
     if (event.keyCode === 71) {
       player.changeG();
     }
+    if (event.keyCode === 82) {
+      window.location.reload();
+    }
   }
 };
 
 let backgroundImage = {
-  img: img,
   x: 0,
   speed: -1,
 
   move: function() {
-    // console.log('move image')
     this.x += this.speed;
     this.x %= 500;
   },
 
   draw: function() {
-    ctx.drawImage(this.img, this.x, 0);
-    // console.log('draw image');
+    ctx.drawImage(img, this.x, 0, 500, 300);
     if (this.speed < 0) {
-      ctx.drawImage(this.img, this.x + 500, 0);
+      ctx.drawImage(img, this.x + 500, 0, 500, 300);
     } else {
-      ctx.drawImage(this.img, this.x - this.img.width, 0);
+      ctx.drawImage(img, this.x - img.width, 0, 500, 300);
     }
   }
 };
 
+let sprite_sheet = {
+  frame_sets: [[3, 6]],
+};
+
 //start game and functions
 
-let player = new Player(20, 30, "red", 55, 250);
+let player = new Player(SPRITE_SIZE, SPRITE_SIZE, "red", 55, 250, new Animation());
 
 function updateGameArea() {
   myGameArea.clear();
@@ -181,30 +386,45 @@ function updateGameArea() {
   backgroundImage.move();
   backgroundImage.draw();
   // drawing ceiling and floor
-  myGameArea.ground();
-  myGameArea.ceiling();
+  ceilingImg.move();
+  ceilingImg.draw();
+  floorImg.move();
+  floorImg.draw();
+
+  if (frames % 240 === 0) {
+    spaceship.addNav();
+  }
+  spaceship.moveNav();
+  spaceship.update();
   // drawing player
-  player.update();
+  player.animation.change(sprite_sheet.frame_sets[0], 8);
+  player.animation.update();
+  player.animation.draw();
   player.applyGforce();
   // creating, moving, and drawing obstacles
   if (frames < 2000) {
+    backgroundImage.speed = -2;
     if (frames % 50 === 0) {
       obstacles.addObs();
     }
   } else if (frames < 4000) {
+    backgroundImage.speed = -3;
     if (frames % 40 === 0) {
       obstacles.addObs();
     }
   } else if (frames < 5200) {
+    backgroundImage.speed = -4;
     if (frames % 30 === 0) {
       obstacles.addObs();
     }
   } else if (frames < 7300) {
+    backgroundImage.speed = -5;
     if (frames % 25 === 0) {
       obstacles.addObs();
     }
   } else {
     if (player.normalG) {
+      backgroundImage.speed = -6;
       player.gravity = 3;
     } else {
       player.gravity = -3;
@@ -217,10 +437,10 @@ function updateGameArea() {
   obstacles.update();
   // animate the canvas
   requestId = window.requestAnimationFrame(updateGameArea);
-  // check if the game should stop
-  // checkGameOver();
   // update and draw the score
-  // myGameArea.score();
+  myGameArea.score();
+  // check if the game should stop
+  checkGameOver();
 }
 
 function checkGameOver() {
